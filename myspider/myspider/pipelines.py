b24f4ad7ff80c868fb5ba17dc2c1b14a3cbd47bc
Mars_i <dwellerofuniverse@gmail.com>
2018-10-09 21:52:11 +0800

    update

--- myspider/myspider/pipelines.py
+++ myspider/myspider/pipelines.py
@@ -8,7 +8,6 @@
 import pymongo
 import re
 from scrapy.exceptions import DropItem
-from scrapy.pipelines.images import ImagesPipeline
 
 def chineseClean(input):
     input = re.sub('\n+', " ", input)
@@ -68,20 +67,4 @@ class MongoPipeline(object):
     def close_spider(self, spider):
         self.client.close()
 
-
-
-class ImagePipeline(ImagesPipeline):
-    def file_path(self, request, response=None, info=None):
-        url = request.url
-        file_name = url.split('/')[-1]
-        return file_name
-
-    def item_completed(self, results, item, info):
-        image_paths = [x['path'] for ok, x in results if ok]
-        if not image_paths:
-            raise DropItem('Image Downloaded Failed')
-        return item 
-
-    def get_media_requests(self, item, info):
-        yield Request(item['url'])
 
